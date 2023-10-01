38e881a385d05985896ddacc63548d0df70a038a
ngokt <ngtt1212@gmail.com>
2023-10-01 23:53:38 +0900

    ユーザーマスタの定義を一部変更

--- db/migrate/20230926142555_create_users.rb
+++ db/migrate/20230926142555_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :mail
-      t.string :password_digest
+      t.string :name, null: false
+      t.string :mail, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
+      t.index :mail, unique: true
     end
   end
 end
