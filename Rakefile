cfbe54f86042f7329df980931ac8918032614e2d
ngokt <ngtt1212@gmail.com>
2024-07-04 23:55:14 +0900

    ログイン機能(1)

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require_relative "config/application"
 
-Rails.application.load_passwords
+Rails.application.load_tasks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_24_143034) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_04_143150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_24_143034) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 
