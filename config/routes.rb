2fd6b5d3cecb11b1bee97d5d791ae533f7972c88
ngokt <ngtt1212@gmail.com>
2024-07-09 00:22:12 +0900

    ユーザー管理用コントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources : users
+  end
   root to: 'passwords#index'
   resources :passwords
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
